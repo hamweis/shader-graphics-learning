8e2756a5e45ac406680cba98f0d5da4783cbc82d
admin <max.hobohm@gmail.com>
2025-08-06 13:29:43 +0200

    minor changes

--- Tutorials/book_of_shaders/uniforms.glsl
+++ Tutorials/book_of_shaders/uniforms.glsl
@@ -6,7 +6,9 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 
 void main() {
-    vec2 st = gl_FragCoord.xy/u_mouse-1.;
+    vec2 st = gl_FragCoord.xy-u_mouse;
+    vec2 norm = st/u_resolution;
+    norm = abs(norm);
 
-    gl_FragColor = vec4(st.xy,0.,1.0);
+    gl_FragColor = vec4(norm.xy,0.,1.0);
 }
