1a1dd6c98ad50f73981a6ad8f9061d7e40d05a4f
Max Hobohm <max.hobohm@gmail.com>
2026-01-14 21:37:19 +0100

    new blank file added

--- new_start.glsl
+++ new_start.glsl
@@ -2,16 +2,18 @@
 precision mediump float;
 #endif
 
-uniform vec2 u_resolution;  // Canvas size (width,height)
-uniform vec2 u_mouse;       // mouse position in screen pixels
-uniform float u_time;       // Time in seconds since load
+uniform vec2 u_resolution;// Canvas size (width,height)
+uniform vec2 u_mouse;// mouse position in screen pixels
+uniform float u_time;// Time in seconds since load
 
 vec4 color(){
-    return vec4(0.0, 1.0, 0.9176, 1.0);
+    return vec4(0.,1.,.9176,1.);
 }
 
-void main() {
-    vec4 given_color= color();
-    //given_color *=abs(sin(u_time*0.5));
-	gl_FragColor = given_color;
+void main(){
+    vec2 normalized_position=gl_FragCoord.xy/u_resolution-.5;
+    float d=length(normalized_position);
+    d=.01/d;
+    d=abs(d);
+    gl_FragColor=vec4(d,0.,0.,1.);
 }
--- random/shader 2.glsl
+++ random/shader 2.glsl
@@ -5,28 +5,27 @@ precision mediump float;
 uniform vec2 u_resolution;
 uniform float u_time;
 
-vec3 palette(float t) {
-    vec3 a = vec3(0.5, 0.5, 0.5);
-    vec3 b = vec3(0.5, 0.5, 0.5);
-    vec3 c = vec3(1.0, 1.0, 1.0);
-    vec3 d = vec3(0.00, 0.10, 0.20);
-    return a + b*cos( 6.283185*(c*t+d) );
+vec3 palette(float t){
+    vec3 a=vec3(.5,.5,.5);
+    vec3 b=vec3(.5,.5,.5);
+    vec3 c=vec3(1.,1.,1.);
+    vec3 d=vec3(0.,.10,.20);
+    return a+b*cos(6.283185*(c*t+d));
 }
 
-void main() {
-    vec4 final_color=vec4(0.0);
-    vec2 uv = (gl_FragCoord.xy*2.0 -u_resolution.xy)/u_resolution.y;
-    vec2 uv2 =uv;
-    uv=fract(uv*sin(u_time)*10.)-0.5;
-    float d = length(uv);
-
+void main(){
+    vec4 final_color=vec4(0.);
+    vec2 uv=(gl_FragCoord.xy*2.-u_resolution.xy)/u_resolution.y;
+    vec2 uv2=uv;
+    //uv=fract(uv*sin(u_time)*10.)-0.5;
+    float d=length(uv);
+    
     d=sin(d*8.+u_time)/8.;
-    d=0.02/d;
+    d=.02/d;
     d=abs(d);
     vec3 color=palette(length(uv2)+u_time);
-
+    
     color*=d;
-
-
-    gl_FragColor=vec4(color, 1.0);
+    
+    gl_FragColor=vec4(color,1.);
 }
